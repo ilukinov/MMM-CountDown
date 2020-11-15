caf10fd816242e3ea4be333da959e8c9e92324dd
ilukinov <illya.lukinov@gmail.com>
2020-11-15 23:09:40 +0200

    fixed css being global

--- MMM-CountDown.js
+++ MMM-CountDown.js
@@ -1,4 +1,4 @@
-Module.register("MMM-CountDown",{
+Module.register("MMM-CountDown", {
 	// Default module config.
 	defaults: {
 		debug: false,
@@ -24,82 +24,83 @@ Module.register("MMM-CountDown",{
 		showMessage: true
 	},
 
-	start: function() {
+	start: function () {
 		var self = this;
 
 		//Set the current event to default. It will be displayed if the are not any events loaded from the calendar
-		this.curEvent  = {date: "3000-01-01", event: "New Millenium", description: "Happy Millenium"};
+		this.curEvent = { date: "3000-01-01", event: "New Millenium", description: "Happy Millenium" };
 		this.events = [];
 
-	
+
 		this.loaded = false;
 		if (this.config.eventDataUrl !== null) {
 			this.countdownLog("Sending message to Get Events");
 			this.sendSocketNotification("EVENTS_GET", {
-				debug: 				 this.config.debug, 
-				eventDataUrl: 		 this.config.eventDataUrl, 
-				auth: 				 this.config.auth,
-				maximumEntries:		 this.config.maximumEntries,
+				debug: this.config.debug,
+				eventDataUrl: this.config.eventDataUrl,
+				auth: this.config.auth,
+				maximumEntries: this.config.maximumEntries,
 				maximumNumberOfDays: this.config.maximumNumberOfDays,
-				reloadInterval: 	 this.config.reloadInterval, 
-				instanceId: 		 this.identifier});
+				reloadInterval: this.config.reloadInterval,
+				instanceId: this.identifier
+			});
 
 			//If multiple events are to be shown
-			if (this.config.numEventsToShow > 0){
+			if (this.config.numEventsToShow > 0) {
 				this.scheduleCarousel();
 			}
 		} else {
 			this.countdownLog("Reading Single Event");
-			this.curEvent = {date: this.config.date, event: this.config.event, description: this.config.description};
+			this.curEvent = { date: this.config.date, event: this.config.event, description: this.config.description };
 			this.loaded = true;
 		}
 
-		 
+
 
 	},
-	
+
 	getStyles: function () {
 		return ["MMM-CountDown.css"];
 	},
 
-	getScripts: function() {
-        return ["moment.js"];
+	getScripts: function () {
+		return ["moment.js"];
 	},
 
-	countdownLog: function(msg){
+	countdownLog: function (msg) {
 		prefix = "MMM-Countdown ";
-		if (this.config.debug){
+		if (this.config.debug) {
 			console.log(prefix + msg);
 		}
 	},
 
-	
+
 
 	// Update function
-	getDom: function() {
+	getDom: function () {
 
-		
+
 		var wrapper = document.createElement("div");
 
-		if (!this.loaded){
+		if (!this.loaded) {
 			wrapper.innerHTML = "Loading . . .";
-            wrapper.classList.add("bright", "light", "small");
-            return wrapper;
+			wrapper.classList.add("bright", "light", "small");
+			return wrapper;
 		}
 
 		//Determine which event to show
-		//if events were loaded  from cal then show the event based on the index set by the carousel 
-		//otherwise show the current event 
+		//if events were loaded  from cal then show the event based on the index set by the carousel
+		//otherwise show the current event
 
-		var event = (this.events.length >0)?this.events[this.eventIndex]:this.curEvent;
-		
+		var event = (this.events.length > 0) ? this.events[this.eventIndex] : this.curEvent;
+
 		wrapper.className = "countDownTimer";
-		
+
 		var header = document.createElement("h1");
-		header.innerHTML=event.event;
-		
+		header.innerHTML = event.event;
+
 		var listWrapper = document.createElement("ul");
-		
+
 		var today = new Date(Date.now());
 		var target = new Date(event.date);
 		var timeDiff = target - today;
@@ -111,83 +112,83 @@ Module.register("MMM-CountDown",{
 		var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
 		if (diffDays < 0) diffDays++;
-		if (diffHours <0) diffHours++;
-		if (diffMinutes <0 ) diffMinutes++;
-		
+		if (diffHours < 0) diffHours++;
+		if (diffMinutes < 0) diffMinutes++;
+
 		// Build the output
 		var hrs = '';
 		var mins = '';
 		var secs = '';
 		var days = '';
-		
+
 		days = '<li><span id="days">' + diffDays + '</span>' + this.config.daysLabel + '</li>';
-		
-		if(this.config.showHours == true) {
-			hrs = '<li><span id="days">' + diffHours + '</span>'  + this.config.hoursLabel + '</li>';
+
+		if (this.config.showHours == true) {
+			hrs = '<li><span id="days">' + diffHours + '</span>' + this.config.hoursLabel + '</li>';
 		}
-		
-		if(this.config.showMinutes == true) {
+
+		if (this.config.showMinutes == true) {
 			mins = '<li><span id="days">' + diffMinutes + '</span>' + this.config.minutesLabel + '</li>';
 		}
-		
-		if(this.config.showSeconds == true) {
+
+		if (this.config.showSeconds == true) {
 			secs = '<li><span id="days">' + diffSeconds + '</span>' + this.config.secondsLabel + '</li>';
 		}
-		
-		
+
+
 		listWrapper.innerHTML = days + hrs + mins + secs;
 
 		wrapper.appendChild(header);
 		wrapper.appendChild(listWrapper);
 
 
-		if (timeDiff <= 0 && event.description && this.config.showMessage){
+		if (timeDiff <= 0 && event.description && this.config.showMessage) {
 			var footer = document.createElement("p");
 			footer.classList.add("medium");
-			footer.innerHTML=event.description;
+			footer.innerHTML = event.description;
 			wrapper.appendChild(footer);
 		}
-		
 
-		
+
+
 		return wrapper;
 	},
 
-	 // this rotates your data
-	 scheduleCarousel: function () {
+	// this rotates your data
+	scheduleCarousel: function () {
 		this.countdownLog("schedule carousle");
 		var self = this;
-        this.rotateInterval = setInterval(() => {
+		this.rotateInterval = setInterval(() => {
 
-			this.eventIndex = (this.eventIndex == this.events.length-1)?0:this.eventIndex + 1;
+			this.eventIndex = (this.eventIndex == this.events.length - 1) ? 0 : this.eventIndex + 1;
 			this.countdownLog(" EventIndex: " + this.eventIndex);
-            this.updateDom(this.config.animationSpeed);
-        }, this.config.rotateInterval);
-    },
+			this.updateDom(this.config.animationSpeed);
+		}, this.config.rotateInterval);
+	},
 
-	socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function (notification, payload) {
 		this.countdownLog("Notification Received: " + notification);
 
 
-		
-		if (notification === "CALENDAR_EVENTS"){
+
+		if (notification === "CALENDAR_EVENTS") {
 
 			if (this.identifier !== payload.id) {
 				return;
 			}
-	
+
 			this.countdownLog("Received " + notification + "set up events" + JSON.stringify(payload.events));
 			this.events = [];
 
-			if (payload.events.length > 0 ){
+			if (payload.events.length > 0) {
 
-				var len = (this.config.numEventsToShow> payload.events.length )?payload.events.length:this.config.numEventsToShow;
-				for (i=0; i<len; i++ ){
+				var len = (this.config.numEventsToShow > payload.events.length) ? payload.events.length : this.config.numEventsToShow;
+				for (i = 0; i < len; i++) {
 					startDate = moment(payload.events[i].startDate, "x").format("YYYY-MM-DD hh:mm a");
 					this.events.push({
-						"date"	: startDate,
-						"event" : payload.events[i].title,
-						"description" : payload.events[i].description
+						"date": startDate,
+						"event": payload.events[i].title,
+						"description": payload.events[i].description
 					});
 				}
 
@@ -196,14 +197,14 @@ Module.register("MMM-CountDown",{
 				this.loaded = true;
 				this.updateDom();
 			}
-		} 
-		else if (notification == "INCORRECT_URL"){
+		}
+		else if (notification == "INCORRECT_URL") {
 			console.error("Invalid URL used for eventDataUrl URL: " + payload.url);
-		} 
-		else if (notification == "FETCH_ERROR"){
+		}
+		else if (notification == "FETCH_ERROR") {
 			//Log error and continue. If everything is set up correctly data will be refreshed once it becames available
 			console.error("Calendar Error. Could not fetch calendar: ", payload.url, payload.error);
-		}		
+		}
 	}
 
 });
